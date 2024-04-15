1f5667534a8aaa19ba71c65b7accd679a9c07dca
zeindevs <zeindevs@gmail.com>
2024-04-15 17:58:55 +0700

    ui: add tw-rocket-crash

---
+++ README.md
@@ -0,0 +1,9 @@
+# CodePen
+
+## UI
+
+- tw-rocket-crash
+
+## License
+
+Copyright (c) 2024 zeindevs. Licensed under the Apache License, Version 2.0 (the "License");
--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
 		rollupOptions: {
 			input: {
 				main: resolve(__dirname, 'index.html'),
+				rocket: resolve(__dirname, 'pages/tw-rocket-crash.html'),
 			},
 		},
 	},
